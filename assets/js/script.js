f283856a45adef4f8f2b26a749517fe1adf89c0f
Cole Enyart <letchro@gmail.com>
2021-09-20 19:41:39 -0500

    Added comments describing functions and their contents

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,71 +1,76 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// global variable
+// asks user what length the password should be and validates if input is a number
+function askHowManyCharacters() {
+  var
+    text = window.prompt("How many characters do you want in your password? (8-128)"),        // ask user for imput
+    number = parseInt(text, 10);                                                              // attempt to convert input into a number, radix 10 select (0-9)
 
-
-/* choose a length of at least 8 characters and no more than 128 characters */
-function askHowManyCharacters(){
-  var text = window.prompt ("How many characters do you want in your password? (8-128)"),
-      number = parseInt(text, 10);
-
-  if( isNaN(number) || number < 8 || number > 128 ) {
-    alert("Input must be at valid number between 8 and 128");
-    return askHowManyCharacters();
+  if (isNaN(number) || number < 8 || number > 128) {                                          // if not valid input
+    alert("Input must be at valid number between 8 and 128");                                 // tell user what is a valid input
+    return askHowManyCharacters();                                                            // ask user for input again
   }
   console.log(number);
-  return number;
+  return number;                                                                              // return valid input
 }
 
+// asks user character set criteria to be chosen
 function askWhichCharacters() {
-  var specialCharsAllowed = window.confirm ("Do you want special characters in your password?"),
-      numbersAllowed = window.confirm ("Do you want numbers in your password?"),
-      lowercaseAllowed = window.confirm ("Do you want lowercase letters in your password?"),
-      uppercaseAllowed = window.confirm ("Do you want uppercase letters in your password?");
-  
-  var special = "",
-      number = "",
-      lower = "",
-      upper = "";
+  var
+    specialCharsAllowed = window.confirm("Do you want special characters in your password?"), // ask user if special characters should be included
+    numbersAllowed = window.confirm("Do you want numbers in your password?"),                 // ask user if numerical characters should be included
+    lowercaseAllowed = window.confirm("Do you want lowercase letters in your password?"),     // ask user if lowercase characters should be included
+    uppercaseAllowed = window.confirm("Do you want uppercase letters in your password?");     // ask user if uppercase characters should be included
 
-  if (specialCharsAllowed) {
-    special = "!@#$^&%*()+=-[]{}|:<>?,.";
-    console.log(special);
+  var                                                                                         // start with an empty character set
+    special = "",
+    number = "",
+    lower = "",
+    upper = "";
+
+  if (specialCharsAllowed) {                                                                  // if user selects special characters (true)
+    special = "!@#$^&%*()+=-[]{}|:<>?,.";                                                     // set special to include all special characters
   }
-  if (numbersAllowed) {
-    number = "1234567890";
-    console.log(number);
+  console.log(special.split(""));
+
+  if (numbersAllowed) {                                                                       // if user selects numerical characters (true)
+    number = "1234567890";                                                                    // set number to include all numbers
   }
-  if (lowercaseAllowed) {
-    lower = "abcdefghijklmnopqrstuvwxyz";
-    console.log(lower);
+  console.log(number.split(""));
+
+  if (lowercaseAllowed) {                                                                     // if user selects lowercase characters (true)
+    lower = "abcdefghijklmnopqrstuvwxyz";                                                     // set lower to include all lowercase characters
   }
-  if (uppercaseAllowed) {
-    upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    console.log(upper);
+  console.log(lower.split(""));
+
+  if (uppercaseAllowed) {                                                                     // if user selects uppercase characters (true)
+    upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";                                                     // set upper to include all uppercase characters
+  }
+  console.log(upper.split(""));
+
+  if (specialCharsAllowed + numbersAllowed + lowercaseAllowed + uppercaseAllowed === 0) {     // if user selects (false) for all characters
+    alert("At least one character type must be selected");                                    // tell user what is a valid input
+    return askWhichCharacters();                                                              // ask user for input again
   }
 
-  if (specialCharsAllowed + numbersAllowed + lowercaseAllowed + uppercaseAllowed === 0) {
-    alert("At least one character type must be selected");
-    return askWhichCharacters();
-  }
-
-  criteria = special + number + lower + upper;
+  criteria = special + number + lower + upper;                                                // combine the selected character sets
   console.log(criteria.split(""));
-  return criteria;
+  return criteria;                                                                            // return combined character sets selected
 }
 
+// generates password based upon criteria chosen by user
 function generatePassword() {
-    var 
-      finalPassword = "",
-      length = askHowManyCharacters(),
-      characters = askWhichCharacters();
+  var
+    finalPassword = "",                                                                       // start with an empty password
+    length = askHowManyCharacters(),                                                          // ask user for length of password to generate
+    characters = askWhichCharacters();                                                        // ask user for what characters to use for password
 
-    for (var i = 0; i < length; i++) {
-      finalPassword += characters[Math.floor(Math.random() * characters.length)];;
-    }
+  for (var i = 0; i < length; i++) {                                                          // add one random character for each character asked for
+    finalPassword += characters[Math.floor(Math.random() * characters.length)];               // add one random character to end of password
+  }
 
-    return finalPassword;
+  return finalPassword;                                                                       // return sequence of random characters
 }
 
 // Write password to the #password input
